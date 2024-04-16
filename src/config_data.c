0232d2a2bc6e9a684d9729fc984a9dbf55b0f9b5
Philip Minakov <sir.cadogan@protonmail.com>
2024-04-16 17:53:18 +0400

    Add default settings for ELKA

--- src/config_data.c
+++ src/config_data.c
@@ -3,15 +3,34 @@
 __CFG_BEGIN(0)
 
 __root CFG_HDR cfghdr_0 = {CFG_STR_WIN1251, "MP CSM addr", 1, 8};
-__root char CFG_MP_CSM_ADDR[9] = "A068ED54";
+#ifdef NEWSGOLD
+    #ifdef ELKA
+        __root char CFG_MP_CSM_ADDR[9] = "A087109C";
+    #else
+        __root char CFG_MP_CSM_ADDR[9] = "A068ED54";
+    #endif
+#endif
+
 
 __root CFG_HDR cfghdr_1 = {CFG_CHECKBOX, "Enable MP illumination", 0, 2};
 __root int CFG_ENABLE_MP_ILLUMINATION = 1;
 
 __root CFG_HDR cfghdr_2 = {CFG_UINT, "Font size clock", 0, 100};
-__root unsigned int CFG_FONT_SIZE_CLOCK = 52;
+#ifdef NEWSGOLD
+    #ifdef ELKA
+        __root unsigned int CFG_FONT_SIZE_CLOCK = 100;
+    #else
+        __root unsigned int CFG_FONT_SIZE_CLOCK = 52;
+    #endif
+#endif
 
 __root CFG_HDR cfghdr_3 = {CFG_UINT, "Font size track", 0, 100};
-__root unsigned int CFG_FONT_SIZE_TRACK = 18;
+#ifdef NEWSGOLD
+    #ifdef ELKA
+        __root unsigned int CFG_FONT_SIZE_TRACK = 28;
+    #else
+        __root unsigned int CFG_FONT_SIZE_TRACK = 18;
+    #endif
+#endif
 
 __CFG_END(0)
